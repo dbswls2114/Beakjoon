80e87a57ce501c84e53a9430857ebe228334b0c5
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2024-05-02 16:18:09 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.15 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cs
@@ -2,11 +2,9 @@ using System;
 
 public class Solution {
     public int solution(int n) {
-       int answer = 1;
-int a = 6;
-while (a % n != 0 )
+        int answer = 1;
+while (6 * answer % n != 0 )
 {
-    a += 6;
     answer++;
 }
 return answer;
