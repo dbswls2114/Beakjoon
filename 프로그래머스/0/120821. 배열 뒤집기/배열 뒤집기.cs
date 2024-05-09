cb179a495be45393a27ab23adb8aee5a07aa87fc
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2024-05-09 22:18:41 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.65 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Linq;
+
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = num_list;
+Array.Reverse(answer);
+        return answer;
+    }
+}
