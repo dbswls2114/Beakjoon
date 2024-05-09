b41a79525656f639336798ce449c849105c4a884
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2024-05-09 22:05:40 +0900

    [level 0] Title: 양꼬치, Time: 0.17 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/양꼬치.cs
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -3,7 +3,7 @@ using System;
 public class Solution {
     public int solution(int n, int k) {
         int answer = 0;
-answer = 12000 * n + (2000 * k - (n / 10 * 2000) <= 0 ? 0 : 2000 * k - (n / 10 * 2000));
+answer = 12000 * n + (k - (n / 10) <= 0 ? 0 : k - (n / 10))*2000;
 return answer;
     }
 }
