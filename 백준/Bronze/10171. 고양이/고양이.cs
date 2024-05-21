97e575575351943624c618bff1062d2b07d035f8
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2024-05-21 23:17:00 +0900

    [Bronze V] Title: 고양이, Time: 124 ms, Memory: 4196 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Day1
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+
+        }
+    }
+}
