93e5c2df0672b1f3a35bf894e06a1d487834f76e
dbswls2114 <54023919+dbswls2114@users.noreply.github.com>
2024-05-23 17:06:59 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.24 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -2,7 +2,7 @@ using System;
 
 public class Solution {
     public int solution(int n) {
-        int answer = (Math.Sqrt(n) % 1 == 0 ? 1 : 2);
+        int answer = Math.Sqrt(n) % 1 == 0 ? 1 : 2;
 return answer;
     }
 }
